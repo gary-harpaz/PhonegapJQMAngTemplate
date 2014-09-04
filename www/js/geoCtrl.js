3705313f556491808014afe8224505afd50aee9e
gary <gary.harpaz@gmail.com>
2014-09-04 22:31:10 +0300

    Signed-off-by: gary <gary.harpaz@gmail.com>

--- www/js/geoCtrl.js
+++ www/js/geoCtrl.js
@@ -1,5 +1,21 @@
 ﻿
 app.controller("geoCtrl", function ($scope) {
-    $scope.firstName = "John";
-    $scope.lastName = "Doe";
+    $scope.getCurrentPosition = function () {
+        navigator.geolocation.getCurrentPosition(
+        function (position) {
+            $.extend($scope, position.coords);
+            $scope.timestamp = position.timestamp;
+            $scope.error_code = "";
+            $scope.error_message = "";
+        },
+        function (positionErrorCallback) {
+            $scope.error_code = positionErrorCallback.code;
+            $scope.error_message = positionErrorCallback.message;
+        }
+        );
+
+    };
+
+
+
 });
