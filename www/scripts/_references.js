b7c1cd8871856ecbf6b376564b24176bc97202a1
gary <gary.harpaz@gmail.com>
2014-09-13 11:08:01 +0300

    Signed-off-by: gary <gary.harpaz@gmail.com>

--- www/scripts/_references.js
+++ www/scripts/_references.js
@@ -1,5 +1,5 @@
 ﻿
 /// <reference path="../scripts/jquery-1.8.0.js" />
-/// <reference path="jquery.mobile-1.4.2.js" />
+/// <reference path="jquery.mobile-1.4.4.js" />
 /// <reference path="../scripts/angular.js" />
 /// <reference path="../js/index.js" />
